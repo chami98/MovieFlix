55d39a4b6a6099a5e82789e636159289330f9b28
chami98 <chamikara98@gmail.com>
2023-12-30 22:06:41 +0530

    Added navigation

---
+++ navigation/appNavigation.js
@@ -0,0 +1,15 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import HomeScreen from "../screens/HomeScreen"
+
+const stack = createNativeStackNavigator()
+
+export default function AppNavigation() {
+    return (
+        <NavigationContainer>
+            <stack.Navigator>
+                <stack.Screen name="Home" options={{ headerShown: false }} component={HomeScreen} />
+            </stack.Navigator>
+        </NavigationContainer>
+    )
+}
