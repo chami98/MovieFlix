2a7afcf9ff75c1fb18837ed666d82e35efd3efaf
chami98 <chamikara98@gmail.com>
2023-12-30 22:29:47 +0530

    created theme styles

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Platform, SafeAreaView, StatusBar, Text, TouchableOpacity, View } from 'react-native'
+import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline'
+const ios = Platform.OS === 'ios'
 
 export default class HomeScreen extends Component {
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
+            <View className="flex-1 bg-neutral-800 ">
+                {/* search bar and logo  */}
+
+                <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
+                    <StatusBar style="light" />
+                    <View className="flex-row justify-between items-center mx-4" >
+                        <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+                        <Text
+                            className="text-white text-3xl font-bold">
+                            MovieFlix
+                        </Text>
+                        <TouchableOpacity>
+                            <MagnifyingGlassIcon size="30" strokeWidth="2" color="white" />
+                        </TouchableOpacity>
+                    </View>
+                </SafeAreaView>
             </View>
         )
     }
