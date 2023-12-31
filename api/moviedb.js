6c008ce8621be0b834292f03aeeb390dd2eacc20
chami98 <chamikara98@gmail.com>
2024-01-01 00:00:44 +0530

    add endpoints

---
+++ api/moviedb.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { apiKey } from '../constants';
+
+// endpoints
+
+// endpoints
+const apiBaseUrl = 'https://api.themoviedb.org/3';
+const trendingMoviesEndpoint = `${apiBaseUrl}/trending/movie/day?api_key=${apiKey}`;
+const upcomingMoviesEndpoint = `${apiBaseUrl}/movie/upcoming?api_key=${apiKey}`;
+const topRatedMoviesEndpoint = `${apiBaseUrl}/movie/top_rated?api_key=${apiKey}`;
+const searchMoviesEndpoint = `${apiBaseUrl}/search/movie?api_key=${apiKey}`;
