878beb1c85c3eb1ab989c6fcf1790b7e2be32cba
chami98 <chamikara98@gmail.com>
2024-01-29 23:34:31 +0530

    Add comments

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-
+// Import the AppNavigation component from the navigation directory
 import AppNavigation from './navigation/appNavigation';
 
+// Define the App component
 export default function App() {
+  // Render the AppNavigation component
   return (
     <AppNavigation />
   );
 }
-
