4859d8512383416071d52c0d5d7bc1431e255393
chami98 <chamikara98@gmail.com>
2024-01-31 23:45:48 +0530

    Add comments

--- components/loading.js
+++ components/loading.js
@@ -1,12 +1,22 @@
+// Import necessary components and libraries from React Native
 import { View, Text, Dimensions } from 'react-native'
 import React from 'react'
+
+// Import Progress components from react-native-progress library
 import * as Progress from 'react-native-progress'
+
+// Import theme object from theme file
 import { theme } from '../theme'
 
+// Get device dimensions
 const { width, height } = Dimensions.get('window')
+
+// Define the Loading component
 export default function Loading() {
     return (
+        // Render a View component to contain the loading indicator
         <View style={{ height, width }} className="absolute flex-row justify-center items-center">
+            {/* Render a Progress CircleSnail component for loading */}
             <Progress.CircleSnail thickness={12} color={theme.background} size={160} />
         </View>
     )
