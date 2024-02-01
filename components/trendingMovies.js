9cc6403433015ba96f6042cb02516797b8280b28
chami98 <chamikara98@gmail.com>
2024-02-01 23:23:25 +0530

    Add comments

--- components/trendingMovies.js
+++ components/trendingMovies.js
@@ -1,18 +1,29 @@
-import { View, Text, TouchableWithoutFeedback, Dimensions, Image } from 'react-native'
-import React from 'react'
-import Carousel from 'react-native-snap-carousel'
+// Import necessary components and libraries from React Native
+import React from 'react';
+import { View, Text, TouchableWithoutFeedback, Dimensions, Image } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 import { useNavigation } from '@react-navigation/native';
 
+// Get device width and height
 var { width, height } = Dimensions.get('window');
 
+// TrendingMovies component definition
 export default function TrendingMovies({ data }) {
+    // Hook into navigation
     const navigation = useNavigation();
+
+    // Handle click event
     const handleClick = (item) => {
-        navigation.navigate('Movie', item)
-    }
+        navigation.navigate('Movie', item);
+    };
+
+    // Render the component
     return (
         <View className="mb-8">
+            {/* Title for trending movies */}
             <Text className="text-white text-xl mx-4 mb-5">Trending</Text>
+
+            {/* Carousel component for trending movies */}
             <Carousel
                 data={data}
                 renderItem={({ item }) => <MovieCard item={item} handleClick={handleClick} />}
@@ -23,21 +34,23 @@ export default function TrendingMovies({ data }) {
                 slideStyle={{ display: 'flex', alignItems: 'center' }}
             />
         </View>
-    )
+    );
 }
 
+// MovieCard component definition
 const MovieCard = ({ item, handleClick }) => {
+    // Render movie card with TouchableWithoutFeedback
     return (
         <TouchableWithoutFeedback onPress={() => handleClick(item)}>
+            {/* Movie poster image */}
             <Image
                 source={require('../assets/images/moviePoster1.png')}
                 style={{
                     width: width * 0.6,
                     height: height * 0.4
-
                 }}
                 className="rounded-xl"
             />
         </TouchableWithoutFeedback>
-    )
-} 
+    );
+};
